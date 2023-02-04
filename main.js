80cd3eee2788bd134c102eaa41d7bb898cedec25
jwke21 <jakevm21@gmail.com>
2023-02-04 09:43:30 -0800

    Added functionality to add map markers

--- main.js
+++ main.js
@@ -8,8 +8,7 @@
 //     return dist_miles * mean;
 // }
 
-
-MAPBOX_ACCESS_TOKEN = "?access_token=pk.eyJ1Ijoid2FrZTIxIiwiYSI6ImNsZHBxNmE5NDAza24zdm1zMmp3b3BlejYifQ.2zh68FrGfBHSDnuhtpk1Ig"
+MAPBOX_ACCESS_TOKEN = "?access_token=pk.eyJ1Ijoid2FrZTIxIiwiYSI6ImNsZHBxNmE5NDAza24zdm1zMmp3b3BlejYifQ.2zh68FrGfBHSDnuhtpk1Ig";
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2FrZTIxIiwiYSI6ImNsZHBxNmE5NDAza24zdm1zMmp3b3BlejYifQ.2zh68FrGfBHSDnuhtpk1Ig';
 const map = new mapboxgl.Map({
@@ -18,24 +17,68 @@ const map = new mapboxgl.Map({
   center: [-122.337410, 47.620630], // starting position [lng, lat]
   zoom: 12, // starting zoom
 });
-// Add control to the map
-map.addControl(
-  new MapBoxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-  })
-);
+
+// Map markers
+var currentMarkers = []
 
 function carbonCalculations() {
-  var coordinates = getCoordinates();
-  console.log(coordinates);
+  var originLat;
+  var originLong;
+  var destLat;
+  var destLong;
+
+  // Clear markers
+  for (var i = currentMarkers.length - 1; i >= 0; i--) {
+    currentMarkers[i].remove();
+  }
+
+  // Get Origin loc
+  var originCoordinates = convertAddressToCoordinates("origin-input");
+  // Get Dest loc
+  var destCoordinates = convertAddressToCoordinates("destination-input");
+
+  var fromLoc;
+  var toLoc;
+
+  originCoordinates.then((res) => {
+    originLat = res[1];
+    originLong = res[0];
+    console.log("Origin: " + originLat + "," + originLong);
+    // Add marker to map
+    const marker = new mapboxgl.Marker()
+      .setLngLat([originLong, originLat])
+      .addTo(map);
+    currentMarkers.push(marker);
+    // Set from location
+    fromLoc = new mapboxgl.LngLat(originLong, originLat);
+  });
+
+  destCoordinates.then((res) => {
+    destLat = res[1];
+    destLong = res[0];
+    console.log("Dest: " + destLat + "," + destLong);
+    // Add marker to map
+    const marker = new mapboxgl.Marker({
+      color: "#FF0000" // Red
+    })
+      .setLngLat([destLong, destLat])
+      .addTo(map);
+    currentMarkers.push(marker);
+    // Set to location
+    toLoc = new mapboxgl.LngLat(destLong, destLat);
+  });
+
+  /* Get dist */
+  var dist;
+  const getDist = setInterval(() => {
+    dist = fromLoc.distanceTo(toLoc);
+  }, 1000);
+
+  /* Perform calculations */
+  
 }
 
-function getCoordinates() {
-    var origin = convertAddressToCoordinates("origin-input");
-    var dest = convertAddressToCoordinates("destination-input");
-    return [origin, dest];
-}
+
 
 // Converts the given address string into coordinates (lattitude and longitude)
 async function convertAddressToCoordinates(elemId) {
@@ -54,9 +97,7 @@ async function convertAddressToCoordinates(elemId) {
     // Make API call
     const response = await fetch(url);
     const myJson = await response.json(); //extract JSON from the http response
-    const val = await JSON.parse(JSON.stringify(myJson));
-    const coordinates = await val.features[0].center;
-    console.log("Coordinates: " + coordinates);
+    var coordinates = await myJson.features[0].center;
     return coordinates;
   } catch (error) {
     console.log(error);
